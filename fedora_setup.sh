360cdc0831fb18ed4544f6ecdf7a0b9a4836c3fb
Ihor Peresunko <marazmatic77@gmail.com>
2016-11-28 22:21:09 +0200

    Added some setups for Fedora

--- fedora_setup.sh
+++ fedora_setup.sh
@@ -21,4 +21,16 @@ dnf install -y htop guake bpython
 echo "${BLUE}Install modules for test-data${NC}"
 dnf install -y redis mongodb
 
+echo "${BLUE}Setting up the git display${NC}"
+curl -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh > ~/.bash_git
+# And add this to the top of your ~/.bashrc
+# source ~/.bash_git
+# export PS1='[\u@\h \W$(declare -F __git_ps1 &>/dev/null && __git_ps1 " (%s)")]\$ '
+
+echo "${BLUE}Install Fira Code${NC}"
+mkdir -p ~/.local/share/fonts
+for type in Bold Light Medium Regular Retina; do wget -O ~/.local/share/fonts/FiraCode-$type.ttf "https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-$type.ttf?raw=true"; done
+fc-cache -f
+
+
 echo "Finished"
